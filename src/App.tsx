e3b57158d76487e9443b4d4721a8fffae7519468
Prabhakar <prabhu115kp@gmail.com>
2022-01-18 21:11:16 +0530

    first

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { List } from './components/List';
+
+
+function App() {
+  return (
+    <div className="App">
+      <List/>
+          </div>
+  );
+}
+
+export default App;
